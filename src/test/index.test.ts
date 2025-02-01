5bdb6c3af40bf274a0bab05a5713acc70a00fe52
xthenuwara <xthenuwara@gmail.com>
2025-02-01 14:53:51 +0530

    ETBS-01-002 - Configure TypeScript, ESLint, and Prettier for code consistency and Jest for testing

---
+++ src/test/index.test.ts
@@ -0,0 +1,21 @@
+import request from "supertest";
+import index from "../index";
+import http from "http";
+
+describe("Health Check Endpoint", () => {
+    let serverInstance: http.Server;
+
+    beforeAll(() => {
+        serverInstance = index.server;
+    });
+
+    afterAll(async () => {
+        await serverInstance.close();
+    });
+
+    it("should return status OK", async () => {
+        const response = await request(serverInstance).get("/health");
+        expect(response.status).toBe(200);
+        expect(response.body.health).toBe("OK");
+    });
+});
